6b3b37b5a024365229243839d685dbe02d5781af
soner urum <soner_urum@hotmail.com>
2022-06-02 16:42:34 +0300

    02.06.2022

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,19 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        
+        public List<ProductDetailDTo> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {   // sql'in linq'de karşılığıdır. Ürünler tablosunu kategori tablosuyla CategoryId'si aynı olanlarla join et 
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDTo 
+                             {ProductId = p.ProductId, ProductName = p.ProductName, 
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList(); 
+            }
+        }
     }
 }
